e07a66775e8fc261a19060df0f5c1aae3cf39bb8
Moritz von Buchwaldt <moritzvonbuchwaldt@web.de>
2019-11-04 12:42:12 +0100

    wazzup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask', as: :ask # 'questions/ask'
+  get 'answer', to: 'questions#answer', as: :answer # 'questions/answer'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+# Rails.application.routes.draw do
+#   get 'about', to: 'pages#about', as: :about
+#   get 'contact', to: 'pages#contact', as: :contact
+
+#   # Generic syntax:
+#   # verb 'path', to: 'controller#action', as: :route_name
+# end
